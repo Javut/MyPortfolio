e1faefd1b4d9fd7c52107db2a3e6f7115f436c11
Javut <javut2010@hotmail.com>
2023-10-24 21:17:39 -0500

    first-commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../assets/styles/Navbar.css';
+
+export const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href='/'>Home</a>
+        </li>
+        <li>
+          <a href='/'>About me</a>
+        </li>
+        <li>
+          <a href='/'>Projects</a>
+        </li>
+        <li>
+          <a href='/'>Skills</a>
+        </li>
+        <li>
+          <a href='/'>Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
